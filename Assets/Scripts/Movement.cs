5cc4065254400897716a49cd3ef82cdf439cfea0
IdanMenaged <idanmenaged@gmail.com>
2024-07-21 00:39:31 +0300

    main menu

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -5,11 +5,13 @@ using UnityEngine;
 public class Movement : MonoBehaviour
 {
     public float speed;
+    
+    private AudioSource sfx;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        sfx = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
@@ -17,5 +19,12 @@ public class Movement : MonoBehaviour
     {
         Vector3 direction = (Input.GetAxisRaw("Vertical") * transform.up + Input.GetAxisRaw("Horizontal") * transform.right).normalized;
         transform.position += direction * speed * Time.deltaTime;
+
+        if (direction == Vector3.zero) {
+            sfx.Stop();
+        }
+        else if (!sfx.isPlaying) {
+            sfx.Play();
+        }
     }
 }
--- Assets/Scripts/RoamAroundTheScreen.cs
+++ Assets/Scripts/RoamAroundTheScreen.cs
@@ -5,24 +5,41 @@ using UnityEngine;
 public class RoamAroundTheScreen : MonoBehaviour
 {
     public float speed;
-
-    private bool currentlyIdle = true;
+    public float stayInPlaceTime;
+    public float startDelay;
 
     private Vector3 minPos;
     private Vector3 maxPos;
+    private Vector3 target;
     
     // Start is called before the first frame update
     void Start()
     {
         minPos = Camera.main.ScreenToWorldPoint(new Vector3(0, 0, 0));
         maxPos = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, 0));
+
+        target = transform.position;
+        StartCoroutine(InitialTarget());
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (currentlyIdle) {
-            currentlyIdle = false;
+        if (transform.position == target) {
+            StartCoroutine(NewTarget());
         }
+        else {
+            transform.position = Vector3.MoveTowards(transform.position, target, speed * Time.deltaTime);
+        }
+    }
+
+    IEnumerator NewTarget() {
+        yield return new WaitForSeconds(stayInPlaceTime);
+        target = new Vector3(Random.Range(minPos.x, maxPos.x), Random.Range(minPos.y, maxPos.y), 0);
+    }
+
+    IEnumerator InitialTarget() {
+        yield return new WaitForSeconds(startDelay);
+        target = new Vector3(Random.Range(minPos.x, maxPos.x), Random.Range(minPos.y, maxPos.y), 0);
     }
 }
